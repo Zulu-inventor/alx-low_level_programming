37ce70bc5601f739679ceb26671311afe38068dc
Zulu-inventor <muteakelvin@gmail.com>
2023-03-16 19:45:33 -0700

    gvjhh

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-/*
+/**
  * main - Entry point of the programme
  * Return - Always 0 (success)
  */
 int main(void)
 {
-	puts("/"programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 
 }
