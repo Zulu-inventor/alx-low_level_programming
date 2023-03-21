7490a3787f742edd40730b6219903a16c6c33c4b
Zulu-inventor <muteakelvin@gmail.com>
2023-03-21 14:26:58 -0700

    bb

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,23 @@
 #include "main.h"
-#include<stdio.h>
 
 /**
- * main-prints _putchar on the screen.
- *
- * Return: Always 0 (success)
+ * main - Entry point
+ * Return: Always 0
  */
+
 int main(void)
+
 {
-	_putchar("\n");
+
+	putchar('');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
+
 }
