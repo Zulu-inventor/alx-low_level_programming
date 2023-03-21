6bb9c2e641968fe13c65e28ca36fc48f09fc99b2
Zulu-inventor <muteakelvin@gmail.com>
2023-03-21 15:15:21 -0700

    hgh

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * main - Entry point
- * Return: Always 0
+ * main - Programme that prints _putchar
+ * Return: Always 0(success)
  */
 
 int main(void)
 
 {
 
-	putchar('');
+	_putchar('-');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
